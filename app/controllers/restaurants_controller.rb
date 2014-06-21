e3d347da4d95a2e1784b375f5558b6a666ecf6d7
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-21 17:43:09 -0400

    Added an 'add' page and new restaurant form.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,4 +8,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      flash.now[:notice] = "Something went wrong, you submission could not be saved."
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :city, :state, :zip_code, :description, :category)
+  end
 end
